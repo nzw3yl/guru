e9c475ecdcc6b5936e6bf7a1fb9aae35ebe9ec47
nzw3yl <roger.smith@ceryx.com>
2013-01-24 13:20:58 -0500

    migrate to pg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,20 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+  
+  before_filter :set_locale
 
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+    # current_user.locale
+    # request.subdomain
+    # request.env["HTTP_ACCEPT_LANGUAGE"]
+    # request.remote_ip
+  end
+
+  def default_url_options(options = {})
+    {locale: I18n.locale}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Guru::Application.routes.draw do
-  authenticated :user do
-    root :to => 'home#index'
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/, :shallow_path => ":locale" do
+      authenticated :user do
+        root :to => 'home#index'
+      end
+      root :to => "home#index"
+      devise_for :users
+      resources :users
   end
-  root :to => "home#index"
-  devise_for :users
-  resources :users
+  match '*path', to: redirect("/#{I18n.default_locale}/%{path}"), constraints: lambda { |req| !req.path.starts_with? "/#{I18n.default_locale}/" }
+  match '', to: redirect("/#{I18n.default_locale}")
 end
